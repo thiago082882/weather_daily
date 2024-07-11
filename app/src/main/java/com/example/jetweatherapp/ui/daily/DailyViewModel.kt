1c9c4b2fc10b99b64b9351ac4d1c2fc2b8c45a02
thiago082882 <thiagocostadejesus2@gmail.com>
2024-07-10 21:57:45 -0300

    view model, dagger, state

--- app/src/main/java/com/example/jetweatherapp/ui/daily/DailyViewModel.kt
+++ app/src/main/java/com/example/jetweatherapp/ui/daily/DailyViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.jetweatherapp.ui.daily
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class DailyViewModel @Inject constructor(): ViewModel(){
 
 }
